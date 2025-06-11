685a39c31c25fc638388f554e7aae50687af3ac2
konstantin <konstantin@mayflower.work>
2025-06-11 20:50:18 +0300

    media files

--- src/game/stories/test.ts
+++ src/game/stories/test.ts
@@ -1,8 +1,9 @@
 import {
-    newActions, newLine,
+    newActions,
     newConversation,
     newHeader,
     newImage,
+    newLine,
     newStory,
     newStoryAnchor,
     newText,
@@ -19,7 +20,7 @@ export const testStory = newStory("testStory", [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     ),
     newImage("living_room.png"),
-    newVideo("bop19.mp4"),
+    newVideo("video.mp4"),
     newActions({
         type: "actions",
         content: [
